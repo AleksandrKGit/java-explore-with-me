0799fe74a852979ab4b9c7956e4c33fe35080201
AleksandrKGit <aleksandr.khandorin@yandex.ru>
2023-03-22 09:50:14 +1000

    feat: ExceptionDto

--- stat/service/src/main/java/ru/practicum/ewm/stat/service/controller/ControllerErrorHandler.java
+++ stat/service/src/main/java/ru/practicum/ewm/stat/service/controller/ControllerErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.stat.service.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
@@ -7,19 +8,24 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import ru.practicum.ewm.common.dto.ExceptionDto;
 import java.time.format.DateTimeParseException;
 
 @ControllerAdvice
 public class ControllerErrorHandler {
     @ExceptionHandler
-    public ResponseEntity<Void> handleException(Exception ignored) {
-        return ResponseEntity.internalServerError().build();
+    public ResponseEntity<ExceptionDto> handleException(Exception exception) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        return new ResponseEntity<>(new ExceptionDto(status, "Stat service error.", exception.getMessage()),
+                status);
     }
 
     @ExceptionHandler({HttpMessageNotReadableException.class, DateTimeParseException.class,
             MissingServletRequestParameterException.class, MethodArgumentTypeMismatchException.class,
             HttpMediaTypeNotSupportedException.class})
-    public ResponseEntity<Void> handleBadRequestException(Exception ignored) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ExceptionDto> handleBadRequestException(Exception exception) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return new ResponseEntity<>(new ExceptionDto(status, "Incorrectly made request.",
+                exception.getMessage()), status);
     }
 }
